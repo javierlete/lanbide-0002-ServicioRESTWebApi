a8a7f6a3a78f256dbd24ee29047004f6748c54b2
Javier Lete <javier@eskura21.com>
2021-10-11 08:55:55 +0200

    Servicio REST básico con WebAPI 2

---
+++ ServicioRESTWebApi/Controllers/ProductosController.cs
@@ -0,0 +1,45 @@
+﻿using Dal;
+using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ServicioRESTWebApi.Controllers
+{
+    public class ProductosController : ApiController
+    {
+        private static readonly IDao<Producto> dao = DaoSqlServerProducto.ObtenerDao();
+        // GET: api/Productos
+        public IEnumerable<Producto> Get()
+        {
+            return dao.ObtenerTodos();
+        }
+
+        // GET: api/Productos/5
+        public Producto Get(long id)
+        {
+            return dao.ObtenerPorId(id);
+        }
+
+        // POST: api/Productos
+        public Producto Post([FromBody]Producto producto)
+        {
+            return dao.Insertar(producto);
+        }
+
+        // PUT: api/Productos/5
+        public Producto Put(long id, [FromBody]Producto producto)
+        {
+            return dao.Modificar(producto);
+        }
+
+        // DELETE: api/Productos/5
+        public void Delete(long id)
+        {
+            dao.Borrar(id);
+        }
+    }
+}
---
+++ ServicioRESTWebApi/Controllers/PruebaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ServicioRESTWebApi.Controllers
+{
+    public class PruebaController : ApiController
+    {
+        public object Get()
+        {
+            return new { Id = 1, Nombre = "Javier" };
+        }
+    }
+}
